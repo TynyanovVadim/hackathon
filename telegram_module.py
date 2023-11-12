dae1244788a1fd35ee2ee49ea9a53dea7c52887e
Vadim <60374867+TynyanovVadim@users.noreply.github.com>
2023-11-12 21:20:06 +0700

    Update telegram_module.py

--- telegram_module.py
+++ telegram_module.py
@@ -6,16 +6,16 @@ from pyrogram import filters
 from pyrogram.types import BotCommand
 from pyrogram.enums import ParseMode
 
-async def search(char_id, words, user):
+async def search(chat_id, words, user):
     client = Client('me_client_bot', 9006209, '57f2dfbe60fb31b1f7ef85ec937f36b5')
 
     await client.start()
     ans = []
     if (user):
-        async for x in client.search_messages(id, query=words, from_user=user):
+        async for x in client.search_messages(chat_id, query=words, from_user=user):
             ans.append({"id": x.id, "from_user": x.from_user, "text": x.text})
     else: 
-        async for x in client.search_messages(id, query=words):
+        async for x in client.search_messages(chat_id, query=words):
             ans.append({"id": x.id, "from_user": x.from_user, "text": x.text})
     await client.stop()
 
